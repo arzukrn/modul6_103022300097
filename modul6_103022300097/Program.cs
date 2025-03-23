7ea7f312f98469008777cb7b907c108c1b94413a
arzukrn <arzukirana17@gmail.com>
2025-03-23 21:10:04 +0700

    implementasi design by contract

--- modul6_103022300097/Program.cs
+++ modul6_103022300097/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - M.ARZU KIRANA BERUTU");
+        video1.PrintVideoDetails();
+
+        video1.IncreasePlayCount(100);
+        video1.PrintVideoDetails();
+    }
+}
+
--- modul6_103022300097/SayaTubeVideo.cs
+++ modul6_103022300097/SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(!string.IsNullOrEmpty(title), "Judul video tidak boleh kosong!");
+        Debug.Assert(title.Length <= 100, "Judul video tidak boleh lebih dari 100 karakter!");
+
         this.title = title;
         Random rand = new Random();
         this.id = rand.Next(10000, 99999);
@@ -20,8 +24,22 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        this.playCount += count;
+        Debug.Assert(count > 0, "Penambahan play count harus lebih dari 0!");
+        Debug.Assert(count <= 10000000, "Penambahan play count tidak boleh lebih dari 10 juta!");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas integer.");
+        }
     }
+
 
     public void PrintVideoDetails()
     {
